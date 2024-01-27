12ae4874a5a3e1243c92ae4773242e38eaff7125
SavitskayaKseniya22 <savitskaya.kseniya.22@yandex.ru>
2024-01-27 17:24:54 +0200

    fix: style for advantages block according to markup

--- src/modules/advantages/advantages.js
+++ src/modules/advantages/advantages.js
@@ -6,10 +6,8 @@ export function advantages() {
   element.id = 'advantages';
 
   const advantagesContent = `
-
       <div class="advantages__elem" id="advantages__elem_comfort">
-        <div class="advantages__text">
-
+        <div class="advantages__text advantages__text_first">
           <h2>комфортно. эргономично. интуитивно.</h2>
           <p>
             Дизайн мыши MX Master 3 разработан так, чтобы с точностью повторять
@@ -17,32 +15,20 @@ export function advantages() {
             большого пальца и расположение кнопок жестов, с которыми вы будете
             творить уверенно как никогда.
           </p>
-
         </div>
-
         <div class="contain__image contain__image_first"></div>
-
       </div>
-
       <div class="advantages__elem">
-
-        <div class="advantages__text" id="advantages__elem_fast">
-
-        
-
+        <div class="advantages__text advantages__text_second" id="advantages__elem_fast">
           <h2>быстрее, чем когда-либо</h2>
           <p>
             Компьютерная мышь MX Master 3 предлагает ультимативный механизм
             прокрутки со скоростью 1000 строк за 1 секунду. С точностью до
             пикселя. Бесшумно.
           </p>
-
         </div>
-
         <div class="contain__image contain__image_second"></div>
-
       </div>
-    
     `;
 
   element.insertAdjacentHTML('afterbegin', advantagesContent);
